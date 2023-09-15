b893acfdcaedb7f26b1ee2cf4992ffdd385b24ff
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-09-14 21:00:00 -0300

    feat: creating the apllication header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import { Github } from 'lucide-react'
 import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <>
-      <div className="">
-        <h1>Hello World</h1>
-        <Button>Testando</Button>
-      </div>
-    </>
+    <div>
+      <header className="flex w-full items-center justify-between border-b px-6 py-3">
+        <h1 className="text-xl font-bold">Upload.AI</h1>
+
+        <nav className="flex items-center gap-5">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+          <Button variant="outline" className="flex items-center gap-2">
+            <Github className="h-4 w-4" />
+            Github
+          </Button>
+        </nav>
+      </header>
+    </div>
   )
 }
